3676e6e4bd7c7cb7da032e57bd2bcdc790cf6108
Bryn Ryans <bryn.ryans@looker.com>
2020-08-20 14:07:15 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@
  * THE SOFTWARE.
  */
 
-import React, { useState } from 'react'
+import React from 'react'
 import { HelloWorld } from './HelloWorld'
 import { ExtensionProvider } from '@looker/extension-sdk-react'
 import { hot } from 'react-hot-loader/root'
